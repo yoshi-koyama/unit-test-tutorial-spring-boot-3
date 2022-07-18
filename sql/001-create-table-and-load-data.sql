533645b72b9be99ffbe67305643c8f82b85cee02
renangton <puccho0427@gmail.com>
2022-07-18 22:39:10 +0900

    DBの初期データをフォーマット

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -1,11 +1,14 @@
 DROP TABLE IF EXISTS anime;
 
-CREATE TABLE anime (
-                        id int unsigned AUTO_INCREMENT,
-                        name VARCHAR(100) NOT NULL,
-                        genre VARCHAR(100) NOT NULL,
-                        PRIMARY KEY(id)
+CREATE TABLE anime
+(
+    id    int unsigned AUTO_INCREMENT,
+    name  VARCHAR(100) NOT NULL,
+    genre VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
 );
 
-INSERT INTO anime (name, genre) VALUES ("鬼滅の刃", "ダークファンタジー");
-INSERT INTO anime (name, genre) VALUES ("SPY×FAMILY", "ホームコメディ");
+INSERT INTO anime (name, genre)
+VALUES ("鬼滅の刃", "ダークファンタジー");
+INSERT INTO anime (name, genre)
+VALUES ("SPY×FAMILY", "ホームコメディ");
