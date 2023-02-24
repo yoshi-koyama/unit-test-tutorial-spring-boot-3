091f2652a5b86075d8e062c96be3319e13efb0d8
小山由人 <koyama@reytech.co.jp>
2023-02-24 23:43:32 +0900

    upgrade to spring boot 3

--- src/main/java/com/example/demo/controller/AnimeController.java
+++ src/main/java/com/example/demo/controller/AnimeController.java
@@ -3,11 +3,18 @@ package com.example.demo.controller;
 import com.example.demo.entity.Anime;
 import com.example.demo.form.AnimeForm;
 import com.example.demo.service.AnimeService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -15,49 +22,49 @@ import java.util.List;
 @RequestMapping(path = "/api/anime")
 public class AnimeController {
 
-    private final AnimeService animeService;
+  private final AnimeService animeService;
 
-    public AnimeController(AnimeService animeService) {
-        this.animeService = animeService;
-    }
+  public AnimeController(AnimeService animeService) {
+    this.animeService = animeService;
+  }
 
-    @GetMapping
-    public List<Anime> getAllAnime() {
-        return animeService.getAllAnime();
-    }
+  @GetMapping
+  public List<Anime> getAllAnime() {
+    return animeService.getAllAnime();
+  }
 
-    @GetMapping(path = "{animeId}")
-    public Anime getAnime(@PathVariable("animeId") Integer id) {
-        return animeService.getAnime(id);
-    }
+  @GetMapping(path = "{animeId}")
+  public Anime getAnime(@PathVariable("animeId") Integer id) {
+    return animeService.getAnime(id);
+  }
 
-    @PostMapping
-    public ResponseEntity<String> registerAnime(@Valid @RequestBody AnimeForm animeForm) {
-        animeService.registerAnime(animeForm.getName(), animeForm.getGenre());
-        URI url = UriComponentsBuilder.fromUriString("http://localhost:8080")
-                .path("api/anime/")
-                .build()
-                .toUri();
-        return ResponseEntity.created(url).body("anime successfully created");
-    }
+  @PostMapping
+  public ResponseEntity<String> registerAnime(@Valid @RequestBody AnimeForm animeForm) {
+    animeService.registerAnime(animeForm.getName(), animeForm.getGenre());
+    URI url = UriComponentsBuilder.fromUriString("http://localhost:8080")
+      .path("api/anime/")
+      .build()
+      .toUri();
+    return ResponseEntity.created(url).body("anime successfully created");
+  }
 
-    @PatchMapping(path = "{animeId}")
-    public ResponseEntity<String> updateAnime(@PathVariable("animeId") Integer id, @Valid @RequestBody AnimeForm animeForm) {
-        animeService.updateAnime(id, animeForm.getName(), animeForm.getGenre());
-        URI url = UriComponentsBuilder.fromUriString("http://localhost:8080")
-                .path("api/anime/" + id)
-                .build()
-                .toUri();
-        return ResponseEntity.created(url).body("anime successfully updated");
-    }
+  @PatchMapping(path = "{animeId}")
+  public ResponseEntity<String> updateAnime(@PathVariable("animeId") Integer id, @Valid @RequestBody AnimeForm animeForm) {
+    animeService.updateAnime(id, animeForm.getName(), animeForm.getGenre());
+    URI url = UriComponentsBuilder.fromUriString("http://localhost:8080")
+      .path("api/anime/" + id)
+      .build()
+      .toUri();
+    return ResponseEntity.created(url).body("anime successfully updated");
+  }
 
-    @DeleteMapping(path = "{animeId}")
-    public ResponseEntity<String> deleteAnime(@PathVariable("animeId") Integer id) {
-        animeService.deleteAnime(id);
-        URI url = UriComponentsBuilder.fromUriString("http://localhost:8080")
-                .path("api/anime/" + id)
-                .build()
-                .toUri();
-        return ResponseEntity.created(url).body("anime successfully deleted");
-    }
+  @DeleteMapping(path = "{animeId}")
+  public ResponseEntity<String> deleteAnime(@PathVariable("animeId") Integer id) {
+    animeService.deleteAnime(id);
+    URI url = UriComponentsBuilder.fromUriString("http://localhost:8080")
+      .path("api/anime/" + id)
+      .build()
+      .toUri();
+    return ResponseEntity.created(url).body("anime successfully deleted");
+  }
 }
--- src/main/java/com/example/demo/form/AnimeForm.java
+++ src/main/java/com/example/demo/form/AnimeForm.java
@@ -1,36 +1,36 @@
 package com.example.demo.form;
 
-import javax.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 
 public class AnimeForm {
 
-    @NotBlank(message = "cannot be empty")
-    private String name;
+  @NotBlank(message = "cannot be empty")
+  private String name;
 
-    @NotBlank(message = "cannot be empty")
-    private String genre;
+  @NotBlank(message = "cannot be empty")
+  private String genre;
 
-    public AnimeForm(String name, String genre) {
-        this.name = name;
-        this.genre = genre;
-    }
+  public AnimeForm(String name, String genre) {
+    this.name = name;
+    this.genre = genre;
+  }
 
-    public AnimeForm() {
-    }
+  public AnimeForm() {
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public String getGenre() {
-        return genre;
-    }
+  public String getGenre() {
+    return genre;
+  }
 
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
+  public void setGenre(String genre) {
+    this.genre = genre;
+  }
 }
